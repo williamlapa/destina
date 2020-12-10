dc8fee48be2cdf78b80c665228f886e18abe7104
william lapa <williamlapa@hotmail.com>
2020-12-10 17:02:20 -0300

    novo request e footer ok

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -11,6 +11,7 @@ class RequestsController < ApplicationController
   def new
     @request = Request.new
     @category = Category.new
+    @categories = Category.all
     # authorize @request
   end
 
