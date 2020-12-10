559436dbd233e56747be42c2ced8b6c8a670857b
william lapa <williamlapa@hotmail.com>
2020-12-10 10:12:28 -0300

    controllers e views basicos

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :requests do
+    resources :orders, only: %i[new create]
+  end
+
+  resources :products do
+    resources :orders, only: %i[new create]
+  end
 end
