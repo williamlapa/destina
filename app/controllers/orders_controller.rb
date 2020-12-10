621010b8a6738ad8419b34b0f442061ad79e43a6
Marcos Costa <marcos.liborio@gmail.com>
2020-12-10 18:05:12 -0300

    Implementando Orders parcial

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :set_request_product
+
   def create
     @order = Order.new(order_params)
+    @order.request = @request
+    @order.product = @product
+    if @order.save
+      render order_path(@order)
+    else
+      render :new
+    end
   end
 
   def new
@@ -11,7 +20,14 @@ class OrdersController < ApplicationController
     @order.update(order_params)
   end
 
+  private
+
   def order_params
-    params.require(:order).permit(:status)
+    params.require(:order).permit(:request_id, :product_id, :status)
+  end
+
+  def set_request_product
+    @request = Request.find(params[:request_id])
+    @product = Product.find(params[:product_id])
   end
 end
